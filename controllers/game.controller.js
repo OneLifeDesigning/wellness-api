f7b4951804afb6136e7115a37a057bf473eb2499
Old <onelifedesigning@gmail.com>
2020-10-24 12:19:48 +0200

    Revert "Fix: Clear token when completed game"
    This reverts commit b8611463a6df5c4bc900435000f8fa45a6c99ac9.

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -72,7 +72,7 @@ module.exports.complete = (req, res, next) => {
     err && next(createError(403));
     UserGame.findOneAndUpdate(
       { token },
-      { token: "", rating, score, comment, isCompleted: true },
+      { rating, score, comment, isCompleted: true },
       { new: true }
     )
       .then(() => res.status(201).json())
