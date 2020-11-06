c22e14f60028ad4939ea88218345a94e8f23627a
1ld <onelifedesigning@gmail.com>
2020-11-06 16:33:29 +0100

    ADD arr cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,15 @@
 const cors = require("cors");
+const whitelist = [process.env.CORS_ORIGIN_ONE, process.env.CORS_ORIGIN_TWO];
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  origin:
+    function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    } || "http://localhost:3000",
   allowedHeaders: ["Content-Type"],
   credentials: true,
 });
