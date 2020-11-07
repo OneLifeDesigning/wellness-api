837f40edb119764b6ce94f1a6739463711802ce5
1ld <onelifedesigning@gmail.com>
2020-11-07 01:15:37 +0100

    Add: Conditional actions on cors and sesson local to producction

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,17 +1,26 @@
 const cors = require("cors");
-const whitelist = [process.env.CORS_ORIGIN_ONE, process.env.CORS_ORIGIN_TWO];
 
-const corsMiddleware = cors({
-  origin:
-    function (origin, callback) {
+if (process.env.DEV === "dev") {
+  const corsMiddleware = cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  });
+  module.exports = corsMiddleware;
+} else {
+  const whitelist = [process.env.CORS_ORIGIN_ONE, process.env.CORS_ORIGIN_TWO];
+
+  const corsMiddleware = cors({
+    origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
-    } || "http://localhost:3000",
-  allowedHeaders: ["Content-Type"],
-  credentials: true,
-});
+    },
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  });
 
-module.exports = corsMiddleware;
+  module.exports = corsMiddleware;
+}
--- config/session.config.js
+++ config/session.config.js
@@ -3,19 +3,35 @@ const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const SESSION_MAX_AGE_SECONDS =
   Number(process.env.SESSION_MAX_AGE_SECONDS) || 60 * 60 * 24 * 7;
-
-module.exports = session({
-  secret: process.env.SESSION_SECRET || "I broke the production chain",
-  resave: true,
-  saveUninitialized: false,
-  cookie: {
-    secure: process.env.SESSION_SECURE,
-    httpOnly: true,
-    maxAge: SESSION_MAX_AGE_SECONDS * 1000,
-    sameSite: "none",
-  },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: SESSION_MAX_AGE_SECONDS,
-  }),
-});
+if (process.env.DEV === "dev") {
+  module.exports = session({
+    secret: process.env.SESSION_SECRET || "I broke the production chain",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: SESSION_MAX_AGE_SECONDS * 1000,
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: SESSION_MAX_AGE_SECONDS,
+    }),
+  });
+} else {
+  module.exports = session({
+    secret: process.env.SESSION_SECRET || "I broke the production chain",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      maxAge: SESSION_MAX_AGE_SECONDS * 1000,
+      sameSite: "none",
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: SESSION_MAX_AGE_SECONDS,
+    }),
+  });
+}
