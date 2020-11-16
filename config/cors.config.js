1e037aa2ad4ee49882e3ecdb3ba243d186eb9b7c
1ld <onelifedesigning@gmail.com>
2020-11-16 19:23:28 +0100

    fix cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,26 +1,24 @@
 const cors = require("cors");
 
-if (process.env.DEV === "dev") {
-  const corsMiddleware = cors({
-    origin: "http://localhost:3000",
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  });
-  module.exports = corsMiddleware;
-} else if (process.env.DEV === "production") {
-  const whitelist = [process.env.CORS_ORIGIN_ONE, process.env.CORS_ORIGIN_TWO];
+// const corsMiddleware = cors({
+//   origin: "http://localhost:3000",
+//   allowedHeaders: ["Content-Type"],
+//   credentials: true,
+// });
+// module.exports = corsMiddleware;
 
-  const corsMiddleware = cors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  });
+const whitelist = [process.env.CORS_ORIGIN_ONE, process.env.CORS_ORIGIN_TWO];
 
-  module.exports = corsMiddleware;
-}
+const corsMiddleware = cors({
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+});
+
+module.exports = corsMiddleware;
